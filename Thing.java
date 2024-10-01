9c96dfa666e84d6e8ffa8ab81ca39707b92f3ffe
christine nguyen <cminhng@cmng-MacBook-Pro.local>
2024-10-01 14:33:35 -0700

    added left/right/maybe Turns + step methods to Thing.java

--- Thing.java
+++ Thing.java
@@ -1,5 +1,7 @@
+import java.util.*;
+
 public abstract class Thing {
-    
+    public Random rand = new Random(System.currentTimeMillis());
     public int  row, col, dir;
     public char lab;
 
@@ -8,6 +10,33 @@ public abstract class Thing {
         this.col = col;
         this.dir = dir;
     }
+
+    public void rightTurn(){
+        dir = (dir + 1) % 4;
+    }
+
+    public void leftTurn(){
+        dir = (dir + 3) % 4;
+    }
+
+    public void maybeTurn(){
+        int i = rand.nextInt(3);
+        if (i == 1) {
+            this.rightTurn();
+          }
+    
+          if (i == 2) {
+            this.leftTurn();
+          }
+    }
+    
+    //what is this
+    public void step(){
+        final int[] dc = {0, 1, 0, -1}, dr = {1, 0, -1, 0};
+        row += dr[dir];
+        col += dc[dir];
+    }
+
 
     public String toString(){
         return row + " " + col + " " + lab;
